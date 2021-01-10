86104bbb2fce618729eea150a5c6c22ee17e219b
dominique2031 <dominique2031@users.noreply.github.com>
2021-01-10 09:56:09 +0800

    creating index.php for the menu/home of the program

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Contact List</title>
+        <link href="main.css" rel="stylesheet"/>
+</head>
+<body>
+    <h1>Mini Contact List</h1>
+    <div class="Menu">
+        <ul class="active">
+            <h2 class="menu">MENU</h2>
+            <li><a href="AddNewContact.php"> Add New Contact </a></li>
+            <li><a href="UpdateContact.php">Edit Contact </a></li>
+            <li><a href="DeleteContact.php">Delete Contact </a></li>
+            <li><a href="ViewContactList.php">View Contact List </a></li>
+        </ul>
+    </div>
+   
+</body>
+</html>
